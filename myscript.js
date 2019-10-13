393c8e9573f7d90c231b4d54448256bc9f1b9dd6
AgrawalHimanshi <himanshi.alg@gmail.com>
2019-10-13 19:48:45 +0530

    added javascript functionality

--- myscript.js
+++ myscript.js
@@ -1,5 +1,32 @@
 //restart game button
+var restart=document.querySelector("#b");
+
 //grab all the squares
+var squares=document.querySelectorAll('td');
 //clear all squares
+
+function clearBoard(){
+	for(var i=0;i<squares.length;i++)
+	{
+		squares[i].textContent='';
+	}
+}
+restart.addEventListener('click',clearBoard);
+
 //clear square marker
+function changeMarker(){
+	if(this.textContent===''){
+		this.textContent='X';
+	}
+	else if(this.textContent==='X'){
+		this.textContent='O';
+	}else{
+		this.textContent='';
+	}
+}
 //for loop to add event listeners to all the squares
+for(var i=0;i<squares.length;i++){
+	squares[i].addEventListener('click',changeMarker)
+}
+
+
