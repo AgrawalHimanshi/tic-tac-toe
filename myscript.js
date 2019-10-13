c0c34ed421f3a7bed7e0c39b144255db44ea1a4a
AgrawalHimanshi <himanshi.alg@gmail.com>
2019-10-14 00:12:24 +0530

    added human player

--- myscript.js
+++ myscript.js
@@ -1,5 +1,71 @@
+var origBoard;
+const huPlayer='O';
+const aiPlayer='X';
+const winCombs=[
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[0,3,6],
+	[1,4,7],
+	[2,5,8],
+	[0,4,8],
+	[6,4,2]
+
+]
+const cells=document.querySelectorAll('.cell');
+startGame();
+function startGame(){
+	document.querySelector(".endgame").style.display="none";
+	origBoard=Array.from(Array(9).keys());
+	for(var i=0;i<cells.length;i++){
+		cells[i].innerText='';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click',turnClick,false);
+	}
+}
+
+function turnClick(square){
+	//console.log(square.target.id)
+	turn(square.target.id,huPlayer);
+}
+function turn(squareId,player){
+	origBoard[squareId]=player;
+	document.getElementById(squareId).innerText=player;
+	let gameWon=checkWin(origBoard,player);
+	if(gameWon) gameOver(gameWon)
+}
+
+function checkWin(board,player){
+	//find all the places on board that are already played in 
+	//a=accumulator initialized to empty array
+	//e=element
+	//i=index
+	let plays=board.reduce((a,e,i)=>
+		(e==player)?a.concat(i):a, []);
+	let gameWon=null;
+	//check if game is won
+	for(let [index,win] of winCombs.entries()){
+		if(win.every(elem=>plays.indexOf(elem)>-1)){
+			gameWon={index:index,player:player};
+				break;
+		}
+	}
+	return gameWon;
+}
+function gameOver(gameWon){
+	for(let index of winCombs[gameWon.index])
+	{
+		document.getElementById(index).style.backgroundColor=
+		gameWon.player==huPlayer? "blue" : "red";
+
+	}
+	for(var i=0;i<cells.length;i++){
+		cells[i].removeEventListener('click',turnClick,false);
+	}
+}
+
 //restart game button
-var restart=document.querySelector("#b");
+/*var restart=document.querySelector("#b");
 
 //grab all the squares
 var squares=document.querySelectorAll('td');
@@ -29,4 +95,4 @@ for(var i=0;i<squares.length;i++){
 	squares[i].addEventListener('click',changeMarker)
 }
 
-
+*/
